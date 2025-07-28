bf9d0bcb769f2f0319c719752dd72c32ced87ce5
iamdipesh18 <dipeshdhungana2002@gmail.com>
2025-07-28 11:14:09 +0545

    Created a list of enums

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,8 @@
+// List of Enums used in the application
+// They cannot be created inside a class as they are not static
+
+enum TextSizes { small, medium, large, xLarge }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethod { paypal, googlePay, applePay, visa, mastercard, creditCard, paystack, razorpay, paytm }
