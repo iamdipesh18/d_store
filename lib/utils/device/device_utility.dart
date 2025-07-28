5208d1409729a3213357e58e61e46d296ded95d4
iamdipesh18 <dipeshdhungana2002@gmail.com>
2025-07-28 12:22:20 +0545

    Added TDeviceUtils for handling device-level utilities (keyboard, screen, orientation, vibration, etc.)

---
+++ lib/utils/device/device_utility.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class TDeviceUtils {
+  static void hideKeyboard(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  static Future<void> setStatusBarColor(Color color) async {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: color, // Set the status bar color
+      ),
+    );
+  }
+
+  static bool isLandscapeOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom == 0;
+  }
+
+  static bool isPortraitOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom != 0;
+  }
+
+  static void setFullScreen(bool enabled) {
+    SystemChrome.setEnabledSystemUIMode(
+      enabled ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge,
+    );
+  }
+
+  static double getScreenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static double getScreenHeight(BuildContext context) {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static double getStatusBarHeight(BuildContext context) {
+    return MediaQuery.of(Get.context!).padding.top;
+  }
+
+  static double getBottomNavigationBarHeight(BuildContext context) {
+    return kBottomNavigationBarHeight;
+  }
+
+  static double getAppBarHeight(BuildContext context) {
+    return kToolbarHeight;
+  }
+
+  static double getNavigationBarHeight(BuildContext context) {
+    return kBottomNavigationBarHeight;
+  }
+
+  static double getPixelRatio(BuildContext context) {
+    return MediaQuery.of(Get.context!).devicePixelRatio;
+  }
+
+  static double getKeyboardHeight(BuildContext context) {
+    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+    return viewInsets.bottom;
+  }
+
+  static Future<bool> isKeyboardVisible(BuildContext context) async {
+    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+    return viewInsets.bottom > 0;
+  }
+
+  static Future<bool> isPhysicalDevice() async {
+    // This method checks if the app is running on a physical device
+    // or an emulator/simulator.
+    return defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS;
+  }
+
+  static void showStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+  }
+
+  static void vibrate(Duration duration) {
+    if (Platform.isAndroid || Platform.isIOS) {
+      HapticFeedback.vibrate();
+      Future.delayed(duration, ()=>HapticFeedback.vibrate() );
+    } else {
+      // For other platforms, you can implement custom vibration logic if needed
+      //print('Vibration not supported on this platform');
+    }
+  }
+
+  static Future<void> setPreferredOrientations(List<DeviceOrientation> orientations) async {
+    await SystemChrome.setPreferredOrientations(orientations);
+  }
+
+  static void hideStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  static Future<bool> hasInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  static bool isAndroid() {
+    return Platform.isAndroid;
+  }
+
+  static bool isIOS() {
+    return Platform.isIOS;
+  }
+
+  static void launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
