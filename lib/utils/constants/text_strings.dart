1ca7e9ef0a5f69c72cb35e61e9eef35432f101b0
iamdipesh18 <dipeshdhungana2002@gmail.com>
2025-07-28 11:10:50 +0545

    Created Text Class which contains all the app text in string format used throughout the application

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,26 @@
+// This class contains all the app text in string format used throughout the application.
+class TText {
+  // -- Global Texts
+  static const String appTitle = 'D Store';
+  static const String appSubtitle = 'Your one-stop shop for everything!';
+
+  // -- OnBoarding Texts
+  static const String onBoardingTitle1 = 'Choose Your Products';  
+  static const String onBoardingTitle2 = 'Select Payment Method';
+  static const String onBoardingTitle3 = 'Deliver at Your Doorstep';
+
+  static const String onBoardingSubtitle1 = 'Welcome to World of Limitless Choices - Your Perfect Product Awaits!';
+  static const String onBoardingSubtitle2 = 'For Seamless Transactions, Choose Your Preferred Payment Path - Your Convenience, Our Priority!';
+  static const String onBoardingSubtitle3 = 'From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!';
+
+  
+  // -- Home 
+  static const String homeAppbarTitle = 'Good day for shopping';
+  static const String homeAppbarSubTitle = 'Dipesh Dhungana';
+
+
+  // -- Error Messages
+
+  // -- Success Messages
+
+}
