8e3445098d81b7f3390d09c791586c491fad9169
iamdipesh18 <dipeshdhungana2002@gmail.com>
2025-07-28 10:49:55 +0545

    Created Size Class and defined sizes for every items that would be included in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:d_store/app.dart';
 import 'package:flutter/material.dart';
+import 'package:d_store/utils/constants/colors.dart';
+import 'package:d_store/utils/theme/theme.dart';
 
 /// Main entry point of the application.
 void main() {
@@ -9,7 +11,6 @@ void main() {
   // Todo: Initialize Firebase
   // Todo: Initialize Authentication
 
-  
 
   runApp(const App());
 }
---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,68 @@
+class TSizes {
+  // Padding and margin sizes
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  // Icon sizes
+  static const double iconXs = 4.0;
+  static const double iconSm = 8.0;
+  static const double iconMd = 16.0;
+  static const double iconLg = 24.0;
+
+  // Font sizes
+  static const double fontSizeSm = 8.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 24.0;
+
+  // Button sizes
+  static const double buttonHeight = 18.0;
+  static const double buttonWidth = 120.0;  
+  static const double buttonRadius = 12.0;
+
+  //AppBar Height
+  static const double appBarHeight = 56.0;
+
+  //Image sizes
+  static const double imageThumbSize = 80.0;
+
+  // Default Spacing Between Sections
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwItems = 16.0;  
+  static const double spaceBtwSections = 32.0;
+
+  // Border Radius
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusMd = 8.0;
+  static const double borderRadiusLg = 12.0;
+
+  // Divider Height
+  static const double dividerHeight = 1.0;
+
+  // Product Item Dimensions
+  static const double productItemHeight = 160.0;
+  static const double productImageRadius = 16.0;
+  static const double productImageSize = 120.0;
+
+  // Input Field Dimensions
+  static const double spaceBtwinputFields = 16.0;
+  static const double inputFieldRadius = 12.0;
+
+  // Card Dimensions
+  static const double cardRadiusLg = 16.0;
+  static const double cardRadiusMd = 12.0;
+  static const double cardRadiusSm = 10.0;
+  static const double cardRadiusXs = 6.0;
+  static const double cardElevation = 2.0;
+
+  // Image Carousel height
+  static const double imageCarouselHeight = 200.0;
+
+  // Loading Indicator Size
+  static const double loadingIndicatorSize = 36.0;
+
+  // Grid View Spacing
+  static const double gridViewSpacing = 16.0;
+}
