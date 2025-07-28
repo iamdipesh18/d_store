e2d63f83d0cebdd4b062ba3b116bc7f2c1d9215d
iamdipesh18 <dipeshdhungana2002@gmail.com>
2025-07-28 10:59:51 +0545

    Created Images Class to store the string value of images that would be used throughout the project

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+// This class contains all the images in string format used throughout the application.
+class TImage {
+  // -- App Logos
+  static const String darkApplogo = 'assets/images/logo.png';
+  static const String lightApplogo = 'assets/images/logo.png';
+
+  // Social Logos
+  static const String facebook = 'assets/images/facebook.png';
+  static const String google = 'assets/images/google.png';  
+  static const String apple = 'assets/images/apple.png';
+}
