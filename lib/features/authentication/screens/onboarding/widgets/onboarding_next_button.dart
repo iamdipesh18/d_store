87090b955d1fae3e6abc67db964d370d9a5ff44e
iamdipesh18 <dipeshdhungana2002@gmail.com>
2025-08-13 12:54:44 +0545

    changed skip button color to applicable one

--- lib/features/authentication/screens/onboarding/widgets/onboarding_next_button.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_next_button.dart
@@ -7,9 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 class OnBoardingNextButton extends StatelessWidget {
-  const OnBoardingNextButton({
-    super.key,
-  });
+  const OnBoardingNextButton({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +16,19 @@ class OnBoardingNextButton extends StatelessWidget {
       right: TSizes.defaultSpace,
       bottom: TDeviceUtils.getBottomNavigationBarHeight(),
       child: ElevatedButton(
-        onPressed: ()=>OnBoardingController.instance.nextPage(),
+        onPressed: () => OnBoardingController.instance.nextPage(),
         style: ElevatedButton.styleFrom(
           shape: const CircleBorder(),
-          backgroundColor: dark? TColors.primary : Colors.black, // Button color
-          borderColor: dark?TColors.primary : Colors.black,
+          backgroundColor: dark
+              ? TColors.primary
+              : Colors.black, // Button color
+          side: BorderSide(
+            color: dark ? TColors.primary : Colors.black,
+            width: 2, // optional
+          ),
         ),
-        child: const Icon(Iconsax.arrow_right_3,),
-        ),
+        child: const Icon(Iconsax.arrow_right_3),
+      ),
     );
   }
 }
